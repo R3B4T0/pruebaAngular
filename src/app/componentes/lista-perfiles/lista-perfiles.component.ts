927933e2473d9398955596c99a998f9fe76ee94b
R3B4T0 <david.rebato14@gmail.com>
2021-02-28 21:44:24 +0100

    subida del dia 28-02-2021

--- src/app/componentes/lista-perfiles/lista-perfiles.component.ts
+++ src/app/componentes/lista-perfiles/lista-perfiles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Mensaje } from 'src/app/clases/mensaje';
 import { User } from 'src/app/clases/user';
 import { MensajeService } from 'src/app/servicios/mensaje.service';
 import { UserService } from 'src/app/servicios/user.service';
@@ -10,8 +11,15 @@ import { UserService } from 'src/app/servicios/user.service';
   styleUrls: ['./lista-perfiles.component.css']
 })
 export class ListaPerfilesComponent implements OnInit {
-  usuarios: any
+  usuarios: [] = []
   usuarioSel: User
+  mensajes: Mensaje[] = []
+  mostrarMensajes: boolean=false;
+  enviarMensajes: boolean = false
+  mensajeSel: Mensaje
+  respuestaCreada: boolean = false
+  errorRespuesta: boolean = false
+  mensaje: string
   formMensaje = this.fb.group({
     id_destinatario: [''],
     mensaje: ['', Validators.required]
@@ -30,15 +38,51 @@ export class ListaPerfilesComponent implements OnInit {
       },
       error => { console.log(error) }
     )
-  } 
+  }
+  
+  obtenerMensajes(): void {
+    this.servicioMensaje.mostrarMensajes().subscribe(
+      respuesta => {
+        console.log(respuesta)
+        this.mensajes = respuesta
+      },
+      error => console.log(error)
+    )
+  }
   
   escribirMensaje(): void {
     this.servicioMensaje.crearMensaje(this.formMensaje.value).subscribe(
       respuesta => {
         console.log(respuesta)
+        this.enviarMensajes = false
+        this.formMensaje.reset()
+        this.respuestaCreada = true
+        this.mensaje = "El mensaje ha sido enviado"
+        setTimeout(() => {this.respuestaCreada = false}, 3000)
       },
-      error => { console.log(error) }
-      
+      error => { 
+        console.log(error) 
+        this.errorRespuesta = true
+        this.mensaje = error.error.error
+        setTimeout(() => {this.errorRespuesta = false}, 3000)
+      }
+    )
+  }
+
+  borrarMensaje(): void {
+    this.servicioMensaje.borrarMensaje(this.mensajeSel.id).subscribe(
+      respuesta => {
+        console.log(respuesta)
+        this.respuestaCreada = true
+        this.mensaje = "El mensaje ha sido borrado"
+        setTimeout(() => {this.respuestaCreada = false}, 3000)
+      },
+      error => {
+        console.log(error)
+        this.errorRespuesta = true
+        this.mensaje = error.error.error
+        setTimeout(() => {this.errorRespuesta = false}, 3000)
+      }
     )
   }
 }
--- src/app/servicios/mensaje.service.ts
+++ src/app/servicios/mensaje.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Mensaje } from '../clases/mensaje';
 
 const url = 'http://localhost/backendphp/mensajes/'
 @Injectable({
